fc2416ff86849b5b94aa03c2c062551a994b4e05
Gabriel <gabriel@MacBook.local>
2023-01-12 00:58:04 -0800

    Modularizando código

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,1 +1,20 @@
 package controllers
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/Gabriel-Newton-dev/Personalidades_API_Rest/database"
+	"github.com/Gabriel-Newton-dev/Personalidades_API_Rest/models"
+)
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Home Page")
+}
+
+func TodasPersonalidades(w http.ResponseWriter, r *http.Request) {
+	var personalidades []models.Personalidade
+	database.DB.Find(&personalidades)
+	json.NewEncoder(w).Encode(personalidades)
+}
--- database/db.go
+++ database/db.go
@@ -1,1 +1,29 @@
 package database
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/Gabriel-Newton-dev/Personalidades_API_Rest/models"
+	"github.com/spf13/viper"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var (
+	DB  *gorm.DB
+	err error
+)
+
+func ConectaComBancoDeDados() {
+	DB_USER := viper.Get("DB_USER")
+	DB_NAME := viper.Get("DB_NAME")
+	DB_PASSWORD := viper.Get("DB_PASSWORD")
+
+	StringDeConexao := fmt.Sprintf("host=localhost user=%s password=%s dbname=%s sslmode=disable", DB_USER, DB_PASSWORD, DB_NAME)
+	DB, err := gorm.Open(postgres.Open(StringDeConexao))
+	if err != nil {
+		log.Fatal(err)
+	}
+	DB.AutoMigrate(&models.Personalidade{})
+}
